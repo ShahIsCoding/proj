436587b7dc725311a35aabf4812ca3797cb88cd9
ShahIsCoding <cpabhishekshah0036@hotmail.com>
2023-11-04 01:27:44 +0530

    used tailwindcss

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,53 +1,77 @@
-import React,{useRef,useState,useEffect} from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
-import { Link } from "react-router-dom"
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import PersonIcon from '@mui/icons-material/Person';
-import ArticleIcon from '@mui/icons-material/Article';
-import BookmarksIcon from '@mui/icons-material/Bookmarks';
-import QueryStatsIcon from '@mui/icons-material/QueryStats';
-export const Navbar = () =>{
-    const[profileDashboradOpen,setProfileDashboardOpen] = useState(false);
+import { Link } from "react-router-dom";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PersonIcon from "@mui/icons-material/Person";
+import ArticleIcon from "@mui/icons-material/Article";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import { Search } from "@mui/icons-material";
+import { BG } from "../../Constants/COLORS";
 
-    const useOutsideAlerter = (ref) => {
-        useEffect(() => {
-            function handleClickOutside(event) {
-              if (ref.current && !ref.current.contains(event.target)) {
-                setProfileDashboardOpen(false);
-              }
-            }
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-              document.removeEventListener("mousedown", handleClickOutside);
-            };
-          }, [ref]);
-    }
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef);
+export const Navbar = () => {
+  const [profileDashboradOpen, setProfileDashboardOpen] = useState(false);
 
-    return(
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light px-2 py-1 border border-bottom  d-flex flex-row justify-content-between">
-                <div className="navbar-brand col-1 text-center">
-                    <Link to="/home">
-                        <img src="https://w7.pngwing.com/pngs/987/778/png-transparent-travelbird-slither-io-logo-android-travelbird-rectangle-logo-business-thumbnail.png" alt="Travel_Bird" />
-                    </Link>
-                </div>
-                <div className="col-md-3 col-6 d-flex justify-content-between align-items-baseline">
-                    <span><ArticleIcon /> Write Post</span>
-                    <span>
-                        <NotificationsIcon className="mx-3"/>
-                        <PersonIcon onClick={()=>setProfileDashboardOpen(true)}/>
-                    </span>
-                </div>
-            </nav>
-            {profileDashboradOpen && 
-            <div className="profileDashboard d-flex flex-column px-4 py-3" ref={wrapperRef}>
-                <span><PersonIcon /> My Profile</span>
-                <span><BookmarksIcon /> Library</span>
-                <span><QueryStatsIcon /> Stats</span>
-                <span>Others</span>         
-            </div>}
-        </>
-    )
-}
+  const useOutsideAlerter = (ref) => {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setProfileDashboardOpen(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
+  return (
+    <>
+      <div
+        className={`grid grid-cols-12 grid-rows-1 p-4 items-center border-b-4 ${BG.blue}`}
+      >
+        <Link to="/home">
+          <img
+            src="https://www.reddiquette.com/wp-content/uploads/2020/09/What-Is-The-Reddit-Logo-Called.png"
+            alt="Reddit_logo"
+            className="h-11"
+          />
+        </Link>
+        <div className={`col-span-5 p-3 rounded-full ${BG.blueLess} mr-1`}>
+          <Search className="" />
+          <input
+            className={`${BG.blueLess}`}
+            value={123}
+            placeholder="Search Anything"
+          />
+        </div>
+        <span
+          className={`col-span-2 col-start-8 ${BG.blueLess} p-3 rounded-full`}
+        >
+          <ArticleIcon /> Write Post
+        </span>
+        <span className="col-span-3 col-start-12 flex justify-around ">
+          <NotificationsIcon className="mx-3" />
+          <PersonIcon onClick={() => setProfileDashboardOpen(true)} />
+        </span>
+      </div>
+      {profileDashboradOpen && (
+        <div className={`w-50 ${BG.blue}`} ref={wrapperRef}>
+          <span>
+            <PersonIcon /> My Profile
+          </span>
+          <span>
+            <BookmarksIcon /> Library
+          </span>
+          <span>
+            <QueryStatsIcon /> Stats
+          </span>
+          <span>Others</span>
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/Constants/COLORS.js
@@ -0,0 +1,8 @@
+export const BG = {
+  blue: "bg-sky-600",
+  blueLess: "bg-sky-500",
+};
+export const TEXT = {
+  blue: "text-sky-600",
+  blueLess: "text-sky-500",
+};
